f833f13ea2653117bda4fad78ac1556e9aaae973
romaprozhoga <romaprozhoga@A25PC3>
2019-08-27 18:54:47 +0300

    logintable

---
+++ WpfApp1/WpfApp1/Contextik.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Contextik:DbContext
+    {
+        public Contextik():base("Constring")
+        {
+                
+        }
+    }
+}
---
+++ WpfApp1/WpfApp1/MemberSchool.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    [Table("tblLoginPanel")]
+   public class MemberSchool
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Log { get; set; }
+        [Required]
+        public string Pass { get; set; }
+
+    }
+}
